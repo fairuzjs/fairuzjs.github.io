16187449844818f9cc64027e36c415ad2a4fcc99
fairuzjs <fairuzeazy@gmail.com>
2024-09-20 21:46:08 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Apa yang pertama kali kita lakukan disaat ketemu ?",
+        answer: "A. Mencari tempat yang enak buat nongkrong",
+        options: [
+            "A. Mencari tempat yang enak buat nongkrong",
+            "B. Mencari jajanan dulu",
+            "C. Berpelukan melepas kangen",
+            "D. Langsung pulang lagi"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa makanan kesukaan aku ?",
+        answer: "C. Vhanchongz Cake",
+        options: [
+            "A. Dimcum",
+            "B. Takoyaki",
+            "C. Vhanchongz Cake",
+            "D. Baso Aci"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Tempat terakhir yang kita kunjungi sebelum aku pulang kerumah ?",
+        answer: "A. Kedai Roti Ibu Saya",
+        options: [
+            "A. Kedai Roti Ibu Saya",
+            "B. Indomaret Cikudapateuh",
+            "C. Saparua",
+            "D. Alun alun Bandoengz"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Minuman yang biasanya aku beli di Indo/Alfa ?",
+        answer: "D. Fruit Tea Freeze & Teh Pucuk",
+        options: [
+            "A. Fruit Tea Freeze",
+            "B. Teh Pucuk",
+            "C. Coca Cola & Fruit Tea ",
+            "D. Fruit Tea Freeze & Teh Pucuk"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Genre film yang aku suka dan harus di tonton ?",
+        answer: "D. Horror & Action",
+        options: [
+            "A. Komedi & Horror",
+            "B. Thriller & Action",
+            "C. Animasi & Komedi",
+            "D. Horror & Action"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Pada tanggal berapa kita pertama kali bertemu ?",
+        answer: "C. 31-Maret-2022",
+        options: [
+            "A. 12-Mei-2022",
+            "B. 01-September-2022",
+            "C. 31-Maret-2022",
+            "D. 30-Maret-2023"
+        ]
+    },    
+    {
+        numb: 7,
+        question: "Tempat nongkrong andalan kita dulu dimana bub ?",
+        answer: "B. Kang Bubur Cikutra",
+        options: [
+            "A. Indomaret Braga",
+            "B. Kang Bubur Cikutra",
+            "C. Yogya Kepatihan",
+            "D. Dimana aja"
+        ]
+    },    
+    {
+        numb: 8,
+        question: "Apa kepanjangan dari nama kampus aku hayoo ?",
+        answer: "A. Institut Teknologi Nasional",
+        options: [
+            "A. Institut Teknologi Nasional",
+            "B. Institut Teknologi Bandung",
+            "C. Institut Teknologi Sepuluh November",
+            "D. Institut Tatitatitut"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Nama jurusan aku apa coba bub ?",
+        answer: "B. Informatika",
+        options: [
+            "A. Sistem Informasi",
+            "B. Informatika",
+            "C. Teknik Komputer",
+            "D. Kebidanan"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Coba tebak nama panjang aku yang benar yang mana ?",
+        answer: "C. Danendra Fairuz Syahla",
+        options: [
+            "A. Danenda Fairuz Corona",
+            "B. Danendra Fairuz Gatot Subroto Supratman",
+            "C. Danendra Fairuz Syahla",
+            "D. Alexander Muhammad Danendra"
+        ]
+    }          
+];
